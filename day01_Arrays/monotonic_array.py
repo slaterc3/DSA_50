fef8470fec5654da6af42fc378bf38633c18d24a
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-08 18:37:24 -0500

    adding monotonic array code

---
+++ day01_Arrays/monotonic_array.py
@@ -0,0 +1,32 @@
+def monotonic_array(array):
+    n = len(array)
+    if n == 0: return True 
+    first = array[0]
+    last = array[-1]
+    
+    if first < last:
+        for k in range(n-1):
+            if array[k] > array[k+1]:
+                return False 
+    elif first == last:
+        for k in range(n-1):
+            if array[k] != array[k+1]:
+                return False 
+    else:
+        for k in range(n-1):
+            if array[k] < array[k+1]:
+                return False 
+    
+    return True 
+
+inc = [1,2,3,4,5]
+inc_broken = [1,2,-3,4,5]
+same = [2,2,2,2,2]
+same_broken = [2,2,3,2,2]
+dec = [5,4,3,2,1]
+dec_broken = [5,4,10,3,1]
+
+lst = [inc, inc_broken, same, same_broken, dec, dec_broken]
+
+for x in lst:
+    print(f"{x} is monotonic?\n{monotonic_array(x)}")
