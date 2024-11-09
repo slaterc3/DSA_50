95090338399a48700799310aae08ac3aa8c8b4b8
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-09 08:02:09 -0500

    centered comments above

--- day01_Arrays/monotonic_array.py
+++ day01_Arrays/monotonic_array.py
@@ -1,7 +1,12 @@
 """Coding Exercise: Monotonic Array
 Question
 
-An array is monotonic if it is either monotone increasing or monotone decreasing. An array is monotone increasing if all its elements from left to right are non-decreasing. An array is monotone decreasing if all  its elements from left to right are non-increasing. Given an integer array return true if the given array is monotonic, or false otherwise."""
+An array is monotonic if it is either monotone increasing or monotone decreasing. 
+An array is monotone increasing if all its elements from left to right 
+are non-decreasing. An array is monotone decreasing if all  its elements 
+from left to right are non-increasing. 
+Given an integer array return true if the given array is 
+monotonic, or false otherwise."""
 
 
 def monotonic_array(array):
