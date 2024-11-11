2ee10be3aada4f39a478ec97937fd4f3bcb633c5
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-11 15:50:08 -0500

    toh+

--- day02_Recursion/tower_of_hanoi.py
+++ day02_Recursion/tower_of_hanoi.py
@@ -5,19 +5,19 @@ The tower of Hanoi is a famous puzzle where we have three rods and N disks. The 
 
 Note: The discs are arranged such that the top disc is numbered 1 and the bottom-most disc is numbered N. Also, all the discs have different sizes and a bigger disc cannot be put on the top of a smaller disc. Refer the provided link to get a better clarity about the puzzle."""
 
-def toh(N, fromm, to, aux):
+def toh(N, fromm, to, aux): 
     count = 1 
-    def helper(N, fromm, to, aux):
+    def helper(N, fromm, to, aux): # call A1
         nonlocal count 
         if N == 1:
             print(f"{count}. move disk {N} from rod {fromm} to rod {to}")
             count += 1 
             return 
-        helper(N-1, fromm, aux, to)
+        helper(N-1, fromm, aux, to) # call B2
         print(f"{count}. move disk {N} from rod {fromm} to rod {to}")
         count += 1
-        helper(N-1, aux, to, fromm)
-    helper(N, fromm, to, aux)
+        helper(N-1, aux, to, fromm) # call C3
+    helper(N, fromm, to, aux) # call D4
     return count == 2 ** N 
 
 x = toh(3, 'A', 'C', 'B')
