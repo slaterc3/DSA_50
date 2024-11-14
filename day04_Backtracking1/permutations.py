8c17decaaee687631e79a2016ade34200d5b5bd8
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-13 21:20:30 -0500

    added description of permutations problem

--- day04_Backtracking1/permutations.py
+++ day04_Backtracking1/permutations.py
@@ -1,4 +1,17 @@
+"""Coding Exercise ( Permutations)
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
+Example 1:
+
+nums = [1,4]
+
+Output :[[1,4],[4,1]]
+
+Example 2:
+
+nums = [1,4,5]
+
+Output :[[1,4,5],[1,5,4],[4,1,5],[4,5,1],[5,1,4],[5,4,1]]"""
 
 def permute(nums):
     if not nums:
