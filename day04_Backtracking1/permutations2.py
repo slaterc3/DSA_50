ab459f8207781f4990b05f979572a35895e77832
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-16 21:42:50 -0500

    changed function name

--- day04_Backtracking1/permutations2.py
+++ day04_Backtracking1/permutations2.py
@@ -1,2 +1,17 @@
 def permuteUnique(nums):
-    pass
+    res = [] 
+    def helper(idx):
+        if idx == len(nums) - 1:
+            res.append(nums[:]) # get copy in res list
+            return 
+        hash_map = {}
+        for j in range(idx, len(nums)):
+            if nums[j] not in hash_map:
+                hash_map[nums[j]] = True 
+                nums[idx], nums[j] = nums[j], nums[idx]
+                helper(idx + 1)
+                nums[idx], nums[j] = nums[j], nums[idx]
+    helper(0)
+    return res 
+    
+print(permuteUnique([1,1,2]))
