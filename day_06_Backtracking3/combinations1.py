2981c3df03529671134ac19d40b7adfec06c91e7
Christopher Guy Slater <cslat22@wgu.edu>
2024-11-19 17:12:11 -0500

    added combinations 1, TODO: test cases

---
+++ day_06_Backtracking3/combinations1.py
@@ -0,0 +1,24 @@
+"""Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+You may return the answer in any order.
+
+Example: n = 4, k=2
+Output =
+
+[[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]"""
+
+def combinations(n, k):
+    res = [] 
+    def helper(start, curr):
+        if len(curr) == k:
+            res.append(curr[:])
+            return 
+        for j in range(start, n+1):
+            curr.append(j)
+            helper(j+1, curr)
+            curr.pop() 
+    helper(0, [])
+    return res 
+
+if __name__ == "__main__":
+    print(combinations(4, 2))
