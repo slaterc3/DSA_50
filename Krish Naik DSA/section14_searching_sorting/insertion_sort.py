5d88f3649bc25c5cc1f1d5741330f4b7976d8a6c
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-21 11:14:32 -0500

    insertion sort

---
+++ Krish Naik DSA/section14_searching_sorting/insertion_sort.py
@@ -0,0 +1,40 @@
+def insertion_sort(arr):
+    # Your code goes here
+    swap = False 
+    n = len(arr)
+    for i in range(1, n):
+        curr = i 
+        current_value = arr[curr]
+        prev = i - 1 
+        prev_value = arr[prev]
+        while arr[curr] < arr[prev] and prev >= 0:
+            # if arr[curr] < arr[prev]:
+            arr[curr], arr[prev] = arr[prev], arr[curr]
+            curr -= 1 
+            prev -= 1 
+            # else:
+            #     swap = False 
+    return arr 
+    
+print(insertion_sort([12,11,13,5,6]))
+
+"""Insertion Sort
+Insertion Sort Algorithm
+
+You are given a list of integers. Write a Python function to sort the list in ascending order using the Insertion Sort algorithm. Insertion Sort works by building a sorted section of the list, one element at a time, by inserting each new element into its proper position within the already sorted section.
+
+Parameters:
+
+lst (List of integers): The list to be sorted.
+
+Returns:
+
+A list of integers sorted in ascending order.
+
+Example:
+
+Input: lst = [12, 11, 13, 5, 6]
+Output: [5, 6, 11, 12, 13]
+
+Input: lst = [31, 41, 59, 26, 41, 58]
+Output: [26, 31, 41, 41, 58, 59]"""
