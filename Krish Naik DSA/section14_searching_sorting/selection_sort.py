c806ba2b7932cf910efbd331343d341d83d69fb4
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-21 09:58:04 -0500

    improved selection sort

--- Krish Naik DSA/section14_searching_sorting/selection_sort.py
+++ Krish Naik DSA/section14_searching_sorting/selection_sort.py
@@ -1,15 +1,25 @@
 def selection_sort(arr):
+    
+    n = len(arr)
+    for i in range(n-1):
+        min_ = i 
+        for j in range(i+1, n):
+            if arr[j] < arr[min_]:
+                min_ = j 
+        arr[i], arr[min_] = arr[min_], arr[i]
+    return arr 
+                
     # Your code goes here
     # minimum = 0 
     # swapped = False 
     # min_ = 0
-    for i in range(len(arr)):
-        # smallest = arr[i]
-        for j in range((i+1), len(arr)):
-            if arr[j] < arr[i]:
-                arr[i], arr[j] = arr[j], arr[i]
-                continue
-    return arr
+    # for i in range(len(arr)):
+    #     # smallest = arr[i]
+    #     for j in range((i+1), len(arr)):
+    #         if arr[j] < arr[i]:
+    #             arr[i], arr[j] = arr[j], arr[i]
+    #             continue
+    # return arr
                 
         
 
