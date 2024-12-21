379c16d1bad61549a53ea33109371ae77f32b8a4
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-21 09:28:45 -0500

    selection sort original answer

---
+++ Krish Naik DSA/section14_searching_sorting/selection_sort.py
@@ -0,0 +1,39 @@
+def selection_sort(arr):
+    # Your code goes here
+    # minimum = 0 
+    # swapped = False 
+    # min_ = 0
+    for i in range(len(arr)):
+        # smallest = arr[i]
+        for j in range((i+1), len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+                continue
+    return arr
+                
+        
+
+lst = [3, 5, 1, 2, 6]
+
+print(selection_sort(lst))
+
+"""Selection Sort
+Selection Sort Algorithm
+
+You are given a list of integers. Write a Python function to sort the list in ascending order using the Selection Sort algorithm. Selection Sort works by repeatedly finding the minimum element from the unsorted part of the list and swapping it with the first element of the unsorted part.
+
+Parameters:
+
+lst (List of integers): The list to be sorted.
+
+Returns:
+
+A list of integers sorted in ascending order.
+
+Example:
+
+Input: lst = [64, 25, 12, 22, 11]
+Output: [11, 12, 22, 25, 64]
+
+Input: lst = [29, 10, 14, 37, 13]
+Output: [10, 13, 14, 29, 37]"""
