9e61bd36ced891d815e0bf9aad4e9591ecdb34f0
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-22 11:03:35 -0500

    first and last alternate solution w/helper functions

--- Krish Naik DSA/section15_binary_search_practice/first_and_last_position.py
+++ Krish Naik DSA/section15_binary_search_practice/first_and_last_position.py
@@ -26,6 +26,50 @@ def searchRange(nums, target):
     
 
 print(searchRange([1,2,3,3,4,5,6,7,7,7], 7))
+# alternate solution
+# def searchRange(nums, target):
+#     """
+#     Find the starting and ending position of a given target value in a sorted array.
+#     Use binary search to find both the first and last occurrence of the target.
+#     """
+#     def findFirst(nums, target):
+#         """
+#         Find the first occurrence of the target using binary search.
+#         """
+#         left, right = 0, len(nums) - 1
+#         result = -1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 result = mid
+#                 right = mid - 1
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#         return result
+ 
+#     def findLast(nums, target):
+#         """
+#         Find the last occurrence of the target using binary search.
+#         """
+#         left, right = 0, len(nums) - 1
+#         result = -1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 result = mid
+#                 left = mid + 1
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#         return result
+ 
+#     start = findFirst(nums, target)
+#     end = findLast(nums, target)
+ 
+#     return [start, end]
 
 """Find First and Last Position of Element in Sorted Array
 Asked in companies
