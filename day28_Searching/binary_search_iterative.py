00c9da56e4951928441493b73599db811da2acd1
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-22 09:12:43 -0500

    adding day28 folder for searching algorithms from the 50 day course

---
+++ day28_Searching/binary_search_iterative.py
@@ -0,0 +1,17 @@
+# T: O(log n)
+# S: O(1) - constant space for iterative solution, O(log n) if recursive
+
+def binary_search(arr, target):
+    left = 0 
+    right = len(arr) - 1 
+    while left <= right:
+        middle = (left + right) // 2 
+        if arr[middle] == target:
+            return middle 
+        elif arr[middle] > target:
+            start = middle + 1 
+        else:
+            end = middle - 1 
+    return -1 
+
+print(binary_search([1,2,3,4,5,6,7,8,9], 9))
