9d27f6fa2d6018cf0230761490583ef14f3f63a4
Christopher Guy Slater <cslat22@wgu.edu>
2024-12-25 12:47:06 -0500

    rotate left

--- Krish Naik DSA/section16_list_array_practice/rotate_list_left.py
+++ Krish Naik DSA/section16_list_array_practice/rotate_list_left.py
@@ -11,5 +11,16 @@ def rotate_left(ARR, D):
     for i in range(len(ARR)):
         lst[(i-D)] = ARR[i]
     return lst 
+
+# def rotate_left(ARR, D):
+#     """
+#     Function to rotate the list to the left by D positions.
+#     :param ARR: List[int] -> The list of integers
+#     :param D: int -> The number of positions to rotate
+#     :return: List[int] -> The list after rotation
+#     """
+#     # Handle the rotation
+#     D = D % len(ARR)  # Handle cases where D is larger than the size of the list
+#     return ARR[D:] + ARR[:D]
     
 print(rotate_left([1,2,3,4,5], 2))
