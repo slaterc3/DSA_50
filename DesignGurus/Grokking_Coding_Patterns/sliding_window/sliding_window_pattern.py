dfd29bf5198b482bbf1c63976af6051772d249fe
Christopher Guy Slater <cslat22@wgu.edu>
2025-01-04 09:37:42 -0500

    added naive solution to sliding window

--- DesignGurus/Grokking_Coding_Patterns/sliding_window/sliding_window_pattern.py
+++ DesignGurus/Grokking_Coding_Patterns/sliding_window/sliding_window_pattern.py
@@ -20,3 +20,26 @@ def main():
 
 
 main()
+
+
+# naive solution
+# class Solution:
+#   def findAverages(self, K, arr):
+#     result = []
+#     for i in range(len(arr)-K+1):
+#       # find sum of next 'K' elements
+#       _sum = 0.0
+#       for j in range(i, i+K):
+#         _sum += arr[j]
+#       result.append(_sum/K)  # calculate average
+
+#     return result
+
+
+# def main():
+#   sol = Solution()
+#   result = sol.findAverages(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+#   print("Averages of subarrays of size K: " + str(result))
+
+
+# main()
