a53c05bedc5b5568651b60bba4310a6b3cac3822
Christopher Guy Slater <cslat22@wgu.edu>
2025-01-14 20:20:38 -0500

    added simple version of quick sort

---
+++ Krish Naik DSA/section26_recursion_search_sort/3. Recursion - Searching and Sorting/quick_sort_fast_way.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr 
+    
+    # get the pivot
+    pivot = arr[-1]
+    
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    
+    return quick_sort(left) + pivot + quick_sort(right)
+
+print(quick_sort([5,4,3,2,1]))
