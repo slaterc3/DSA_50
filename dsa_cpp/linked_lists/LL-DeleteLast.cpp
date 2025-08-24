35eeae58e5a6fecc32e71c6bc665a200c1ae99c4
Christopher Guy Slater <cslat22@wgu.edu>
2025-08-23 21:04:41 -0400

    adding DeleteLast function to LL

---
+++ dsa_cpp/linked_lists/LL-DeleteLast.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+}; 
+
+
+class LinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        LinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        Node* getTail() {
+            return tail; 
+        }
+
+        int getLength() {
+            return length;
+        }
+        
+        // We will use this method to test deleteLast on an empty list
+        void makeEmpty() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+            tail = nullptr;
+            length = 0;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
+        
+        void deleteLast() {
+            if (head==nullptr){
+                return;
+            }
+            Node* temp = tail;
+            if (length==1){
+                head = nullptr;
+                tail = nullptr;
+            }
+            else{
+                Node* prev = head;
+                while (prev->next!=tail){
+                    prev = prev->next;
+                }
+                tail = prev;
+                tail->next = nullptr;
+            }
+            length--;
+        }
+        
+
+};
+
+
