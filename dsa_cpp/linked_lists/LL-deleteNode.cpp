bb02adf5dafe306b953678da13c6e33d20f7e7ed
Christopher Guy Slater <cslat22@wgu.edu>
2025-08-28 18:37:37 -0400

    adding several LL methods

---
+++ dsa_cpp/linked_lists/LL-deleteNode.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+}; 
+
+
+class LinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        LinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        Node* getTail() {
+            return tail; 
+        }
+
+        int getLength() {
+            return length;
+        }
+        
+        // We will use this method to test deleteNode on an empty list
+        void makeEmpty() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+            tail = nullptr;
+            length = 0;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        void deleteLast() {
+            if (length == 0) return;
+            Node* temp = head;
+            if (length == 1) {
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                Node* pre = head;
+                while (temp->next) {
+                    pre = temp;
+                    temp = temp->next;
+                }
+                tail = pre;
+                tail->next = nullptr;
+            }
+            delete temp;
+            length--;
+        }
+
+        void prepend(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode->next = head;
+                head = newNode;
+            }
+            length++;
+        }
+
+        void deleteFirst() {
+            if (length == 0) return;
+            Node* temp = head;
+            if (length == 1) {
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                head = head->next;
+            }
+            delete temp;
+            length--;
+        }
+
+        Node* get(int index) {
+            if (index < 0 || index >= length) return nullptr;
+            Node* temp = head;
+            for (int i = 0; i < index; ++i) {
+                temp = temp->next;
+            }
+            return temp;
+        }
+
+        bool set(int index, int value) {
+            Node* temp = get(index);
+            if (temp) {
+                temp->value = value;
+                return true;
+            } 
+            return false;
+        }
+
+        bool insert(int index, int value) {
+            if (index < 0 || index > length) return false;
+            if (index == 0) {
+                prepend(value);
+                return true;
+            }
+            if (index == length) {
+                append(value);
+                return true;
+            }
+            Node* newNode = new Node(value);
+            Node* temp = get(index - 1);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            length++;
+            return true;
+        }       
+        
+        void deleteNode(int index){
+            if (index < 0 || index >= length) return;
+            
+            if (index == 0){
+                deleteFirst();
+                return;
+            }
+            if (index == length-1){
+                deleteLast();
+                return;
+            } else{
+                Node* pre = get(index-1);
+                Node* temp = pre->next;
+                Node* after = temp->next;
+                
+                pre->next = after;
+                temp->next = nullptr;
+                
+                delete temp;
+                length--;
+            }
+        }
+        
+        //   +======================================================+
+        //   |                 WRITE YOUR CODE HERE                 |
+        //   | Description:                                         |
+        //   | - Delete a node at a specific index                  |
+        //   | - Return type: void                                  |
+        //   |                                                      |
+        //   | Tips:                                                |
+        //   | - Use get to find node before index                  |
+        //   | - Special cases: index is 0 or equal to length - 1   |
+        //   | - Delete node if found                               |
+        //   | - Adjust 'next' pointers of adjacent nodes           |
+        //   | - Reduce length by 1                                 |
+        //   | - Check output from Test.cpp in "User logs"          |
+        //   +======================================================+
+
+};
+
+
