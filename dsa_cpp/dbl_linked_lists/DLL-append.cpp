28538c238749aa3e6ab0c45ab896778945a78ddf
Christopher Guy Slater <cslat22@wgu.edu>
2025-09-03 22:08:26 -0400

    append operation on Dbly-LinkedList

---
+++ dsa_cpp/dbl_linked_lists/DLL-append.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public: 
+        int value;
+        Node* next;
+        Node* prev;
+    
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        DoublyLinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+    
+        ~DoublyLinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+    
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty" << endl;
+                return;
+            }
+            while (temp->next != nullptr) {
+                cout << temp->value << " <-> ";
+                temp = temp->next;
+            }
+            cout << temp->value << endl;
+        }
+    
+        Node* getHead() {
+            return head;
+        }
+    
+        Node* getTail() {
+            return tail;
+        }
+    
+        int getLength() {
+            return length;
+        }
+        
+        // This method will make the DLL empty.
+        // It will be used to make sure append works with an empty DLL.
+        void makeEmpty() {
+            Node* current = head;
+            while (current != nullptr) {
+                Node* nodeToDelete = current;
+                current = current->next;
+                delete nodeToDelete;
+            }
+            head = nullptr;
+            tail = nullptr;
+            length = 0;
+        }
+        
+        void append(int value){
+            Node* newNode = new Node(value);
+            if (length == 0){
+                head = newNode;
+                tail = newNode;
+                
+            }else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+        //   +=====================================================+
+        //   |                 WRITE YOUR CODE HERE                |
+        //   | Description:                                        |
+        //   | - Write code to append 'value' to the doubly linked |
+        //   |   list                                              |
+        //   | - Return type: void                                 |
+        //   |                                                     |
+        //   | Tips:                                               |
+        //   | - Consider edge cases like empty lists              |
+        //   | - Remember to update the 'length' and 'tail'        |
+        //   | - Use the 'prev' pointer to maintain the backward   |
+        //   |   connection                                        |
+        //   | - Check output from Test.cpp in "User logs"         |
+        //   +=====================================================+
+    
+};
+
+
