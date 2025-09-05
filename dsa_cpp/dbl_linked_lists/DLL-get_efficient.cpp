763821a8ba5f2eae178d05b248ba9a139b5abfee
Christopher Guy Slater <cslat22@wgu.edu>
2025-09-05 11:38:09 -0400

    more efficient get() method for DLL

---
+++ dsa_cpp/dbl_linked_lists/DLL-get_efficient.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public: 
+        int value;
+        Node* next;
+        Node* prev;
+    
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        DoublyLinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+    
+        ~DoublyLinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+    
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty" << endl;
+                return;
+            }
+            while (temp->next != nullptr) {
+                cout << temp->value << " <-> ";
+                temp = temp->next;
+            }
+            cout << temp->value << endl;
+        }
+    
+        Node* getHead() {
+            return head;
+        }
+    
+        Node* getTail() {
+            return tail;
+        }
+    
+        int getLength() {
+            return length;
+        }
+        
+        // This method will make the DLL empty.
+        // It will be used to make sure get works with an empty DLL.
+        void makeEmpty() {
+            Node* current = head;
+            while (current != nullptr) {
+                Node* nodeToDelete = current;
+                current = current->next;
+                delete nodeToDelete;
+            }
+            head = nullptr;
+            tail = nullptr;
+            length = 0;
+        }
+    
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+    
+        void deleteLast() {
+            if (length == 0) return;
+            Node* temp = tail;
+            if (length == 1) {
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                tail = tail->prev;
+                tail->next = nullptr;
+            }
+            delete temp;
+            length--;
+        }
+    
+        void prepend(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+            }
+            length++;
+        }
+    
+        void deleteFirst() {
+            if (length == 0) return;
+            Node* temp = head;
+            if (length == 1) {
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                head = head->next;
+                head->prev = nullptr;
+            }
+            delete temp;
+            length--;
+        }
+        
+        Node* get(int index) {
+            if (index < 0 || index >= length) return nullptr;
+            
+            Node* curr = head;
+            if (index < length / 2) {
+                // Node* curr = head;
+                for (int i = 0; i < index; i++) {
+                    curr = curr->next;
+                }
+            } else {
+                curr = tail;
+                for (int i = length-1; i > index; i--) {
+                    curr = curr->prev;
+                }
+            }
+            return curr;
+        }
+        
+        //   +=====================================================+
+        //   |                 WRITE YOUR CODE HERE                |
+        //   | Description:                                        |
+        //   | - Write code to get a node at a given index in the  |
+        //   |   doubly linked list.                               |
+        //   | - Return type: Node* (Pointer to the node)           |
+        //   |                                                     |
+        //   | Tips:                                               |
+        //   | - Handle invalid indices by returning nullptr.      |
+        //   | - Use a for loop to iterate through the list.       |
+        //   | - Use the 'length' variable to optimize your loop.  |
+        //   | - Check output from Test.cpp in "User logs".        |
+        //   +=====================================================+
+    
+};
+
+
